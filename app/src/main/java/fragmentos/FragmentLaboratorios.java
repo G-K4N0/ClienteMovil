7cdd26dae62fa48a1224e8fbb099c6834c6939f6
K4T0 <talentounido.kano@gmail.com>
2023-04-26 17:57:04 -0600

    Labs

--- app/src/main/java/fragmentos/FragmentLaboratorios.java
+++ app/src/main/java/fragmentos/FragmentLaboratorios.java
@@ -1,44 +1,47 @@
 package fragmentos;
 
+import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.talentounido.cliente.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentLaboratorios#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import PeticionesVolley.Peticiones;
+import adaptadores.AdapterRecyclerLabs;
+import modelo.Lab;
+
 public class FragmentLaboratorios extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+    private OnFragmentInteractionListener mListener;
+
+    RecyclerView recyclerLabs;
+    ArrayList<Lab> arrayLabs;
     public FragmentLaboratorios() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentLaboratorios.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentLaboratorios newInstance(String param1, String param2) {
         FragmentLaboratorios fragment = new FragmentLaboratorios();
         Bundle args = new Bundle();
@@ -57,10 +60,63 @@ public class FragmentLaboratorios extends Fragment {
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_laboratorios, container, false);
+        View vista = inflater.inflate(R.layout.fragment_laboratorios, container, false);
+        recyclerLabs = vista.findViewById(R.id.idRecycler);
+        recyclerLabs.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        llenarLista();
+
+        return vista;
+    }
+
+    public void llenarLista(){
+        String token = Peticiones.getToken(getActivity());
+        Peticiones.getLabs(token,response->{
+            arrayLabs = new ArrayList<>();
+            for (int i = 0; i < response.length(); i++){
+                try {
+                    JSONObject labDataJson = response.getJSONObject(i);
+                    arrayLabs.add(new Lab(labDataJson.getInt("id"),labDataJson.getString("name"), labDataJson.getBoolean("ocupado")));
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            AdapterRecyclerLabs adapter = new AdapterRecyclerLabs(arrayLabs);
+            recyclerLabs.setAdapter(adapter);
+            adapter.setOnClickListener(this::pasarData);
+        },error -> {
+            Toast.makeText(getContext(), "No se puede establecer conexion con el servidor", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void pasarData(View view){
+        int position = recyclerLabs.getChildAdapterPosition(view);
+        Lab labSeleccionado = arrayLabs.get(position);
+
+        DetallesLab detallesLab = new DetallesLab();
+        Bundle bundle = new Bundle();
+        bundle.putString("nombreLab", labSeleccionado.getName());
+        bundle.putBoolean("statusLab", labSeleccionado.getStatus());
+        detallesLab.setArguments(bundle);
+
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameContainer, detallesLab).addToBackStack(null).commit();
+    }
+    public void onButtonPressed(Uri uri){
+        if (mListener != null){
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener{
+        void onFragmentInteraction(Uri uri);
     }
 }
