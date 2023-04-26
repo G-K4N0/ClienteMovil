785fbc6b3900d463650acaccba6a9eaaface1c49
K4T0 <talentounido.kano@gmail.com>
2023-04-26 17:56:43 -0600

    Inicio

--- app/src/main/java/com/talentounido/cliente/MainActivity.java
+++ app/src/main/java/com/talentounido/cliente/MainActivity.java
@@ -3,10 +3,13 @@ package com.talentounido.cliente;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import fragmentos.FragmentLaboratorios;
+
+public class MainActivity extends AppCompatActivity implements FragmentLaboratorios.OnFragmentInteractionListener {
 private Button btnIniciar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +20,18 @@ private Button btnIniciar;
     public void setBtnIniciar(){
         btnIniciar = findViewById(R.id.btnIniciar);
         btnIniciar.setOnClickListener(v -> {
-            Intent Login = new Intent(MainActivity.this, Login.class);
-            startActivity(Login);
+            Intent home = new Intent(MainActivity.this, Home.class);
+            startActivity(home);
         });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
