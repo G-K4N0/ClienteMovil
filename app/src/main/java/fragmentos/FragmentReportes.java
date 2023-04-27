966152723cc505de9ba7c74c228374ea8b4358d8
K4T0 <talentounido.kano@gmail.com>
2023-04-27 17:54:04 -0600

    Union de elemento de vista y código

--- app/src/main/java/fragmentos/FragmentReportes.java
+++ app/src/main/java/fragmentos/FragmentReportes.java
@@ -7,38 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.talentounido.cliente.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentReportes#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentReportes extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private EditText txtLaboratorios,txtProblema, txtDescripcion;
+    private Button btnReportar;
 
     public FragmentReportes() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentReportes.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentReportes newInstance(String param1, String param2) {
         FragmentReportes fragment = new FragmentReportes();
         Bundle args = new Bundle();
@@ -60,7 +47,21 @@ public class FragmentReportes extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reportes, container, false);
+        View view = inflater.inflate(R.layout.fragment_reportes, container, false);
+        btnReportar = view.findViewById(R.id.btnReportar);
+        txtLaboratorios = view.findViewById(R.id.txtOpciones_lab);
+        txtProblema = view.findViewById(R.id.txtReporte_problema);
+        txtDescripcion = view.findViewById(R.id.txtReporte_descripcion);
+        click();
+        return view;
+    }
+
+    private void click(){
+        btnReportar.setOnClickListener(view -> {
+            String lab = txtLaboratorios.getText().toString();
+            String problema = txtProblema.getText().toString();
+            String descripcion = txtDescripcion.getText().toString();
+            Toast.makeText(getActivity(), lab + problema + descripcion, Toast.LENGTH_SHORT).show();
+        });
     }
 }
