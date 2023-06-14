e89619ee32c2fa0261279cce9f0111028591e74d
K4T0 <talentounido.kano@gmail.com>
2023-06-14 15:43:28 -0600

    Inicio, refresh de horarios

--- app/src/main/java/com/talentounido/cliente/fragmentos/FragmentInicio.java
+++ app/src/main/java/com/talentounido/cliente/fragmentos/FragmentInicio.java
@@ -1,10 +1,11 @@
 package com.talentounido.cliente.fragmentos;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,31 +19,27 @@ import com.talentounido.cliente.R;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 import com.talentounido.cliente.PeticionesVolley.Peticiones;
 import com.talentounido.cliente.adaptadores.AdapterLabsInicio;
+import com.talentounido.cliente.funciones.Tiempo;
 import com.talentounido.cliente.modelo.HorarioActual;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentInicio#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentInicio extends Fragment {
 
-    private ListView listView;
     private Adapter adapterLabs;
     private ArrayList<HorarioActual> horarioActuals;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
 
     public FragmentInicio() {
 
@@ -58,31 +55,51 @@ public class FragmentInicio extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_inicio, container, false);
-        listView = view.findViewById(R.id.lst_inicio_labs);
+        ListView listView = view.findViewById(R.id.lst_inicio_labs);
+
+        adapterLabs = new AdapterLabsInicio(new ArrayList<>(), getActivity());
+        listView.setAdapter((ListAdapter) adapterLabs);
+        horarioActuals = new ArrayList<>();
 
         getHorarioActuals(listView);
-        String fecha = obtenerFechaActual();
-        Toast.makeText(getActivity(), fecha, Toast.LENGTH_SHORT).show();
+
+
+        final Handler handler = new Handler(Looper.myLooper());
+        final int delay =  30 * 60 * 1000;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                actualizarDatos(listView);
+
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+
         return view;
     }
 
-    private void getHorarioActuals(ListView listView){
-        String token = Peticiones.getToken(getActivity());
+
+    private void actualizarDatos(ListView listView) {
+        synchronized (adapterLabs) {
+            horarioActuals.clear();
+            adapterLabs.notifyAll();
+        }
+        getHorarioActuals(listView);
+    }
+
+    private void getHorarioActuals(ListView listView) {
+
+        if (!isAdded()) {
+            return;
+        }
+        String token = Peticiones.getPreference(getActivity(), getString(R.string.token));
         horarioActuals = new ArrayList<>();
         Peticiones.getHorariosByDias(token, response -> {
-            for (int i = 0; i < response.length(); i++){
+            for (int i = 0; i < response.length(); i++) {
                 try {
                     JSONObject dataJson = response.getJSONObject(i);
                     String inicio = dataJson.getString("inicia");
@@ -94,36 +111,28 @@ public class FragmentInicio extends Fragment {
                     String docente = dataJson.getString("docente");
                     String image = dataJson.getString("image");
                     int ocupado = dataJson.getInt("ocupado");
-                    boolean isOcupado;
-                    isOcupado = ocupado >= 1;
+                    boolean isOcupado = ocupado >= 1;
 
-                    horarioActuals.add(new HorarioActual(inicio, finaliza,laboratorio,materia,carrera,grupo,docente, image, isOcupado));
-                }catch(JSONException e){
+                    if (hoursFilter(inicio, finaliza)) {
+                        horarioActuals.add(new HorarioActual(inicio, finaliza, laboratorio, materia, carrera, grupo, docente, image, isOcupado));
+                    }
+                } catch (JSONException e) {
                     Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_SHORT).show();
                 }
             }
-            adapterLabs = new AdapterLabsInicio(horarioActuals, getActivity());
-            listView.setAdapter((ListAdapter) adapterLabs);
+            if (horarioActuals.isEmpty()) {
+                Toast.makeText(getActivity(), "No hay horarios asignados en este momento", Toast.LENGTH_SHORT).show();
+            } else {
+                adapterLabs = new AdapterLabsInicio(horarioActuals, getActivity());
+                listView.setAdapter((ListAdapter) adapterLabs);
+            }
         }, error -> Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_SHORT).show());
     }
-    private String obtenerFechaActual() {
-        Date date = new Date();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE", Locale.getDefault());
-        String dayOfWeek = dateFormat.format(calendar.getTime());
-
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH) + 1;
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-        int second = calendar.get(Calendar.SECOND);
-
-        @SuppressLint("DefaultLocale") String dateTime = String.format("%s, %02d/%02d/%04d %02d:%02d:%02d", dayOfWeek, day, month, year, hour, minute, second);
-
-        return dateTime;
+    private boolean hoursFilter(String initHourRange, String finalHourRange) {
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.O) {
+            return false;
+        }
+        return Tiempo.inInterval(initHourRange, finalHourRange);
     }
 }
