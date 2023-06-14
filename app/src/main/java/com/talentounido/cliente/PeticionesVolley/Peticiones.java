6b39e9cfaec2214ad2e98d77f6c84a17f97bee23
K4T0 <talentounido.kano@gmail.com>
2023-06-14 15:52:00 -0600

    Peticiones grupos,actividades y crear Registro sin horario

--- app/src/main/java/com/talentounido/cliente/PeticionesVolley/Peticiones.java
+++ app/src/main/java/com/talentounido/cliente/PeticionesVolley/Peticiones.java
@@ -21,11 +21,13 @@ import java.util.Map;
 
 import com.talentounido.cliente.modelo.RegisterContent;
 import com.talentounido.cliente.modelo.Reporte;
+import com.talentounido.cliente.modelo.SinRegisterContent;
 
 public class Peticiones {
-    private static final String url1 = "http://192.168.3.117:8080/";
+    private static final String url1 = "http://192.168.137.139:8080/";
+    private static final String url2 = "http://192.168.2.41:8080/";
     private static final String remoteUrl="https://centroapi.azurewebsites.net/";
-    private static final String BASE_URL = remoteUrl;
+    private static final String BASE_URL = url2;
 
     private static RequestQueue peticion;
 
@@ -95,13 +97,54 @@ public class Peticiones {
         peticion.add(request);
     }
 
+    public static void getGrupos(String token, Response.Listener<JSONArray> successListener, Response.ErrorListener errorListener){
+        String url = BASE_URL + "grupos/grupos";
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url,null,successListener, errorListener){
+          @Override
+          public  Map<String, String> getHeaders(){
+              Map<String, String> headers = new HashMap<>();
+              headers.put("Authorization", "Bearer " + token);
+              return headers;
+          }
+        };
+        peticion.add(request);
+    }
+
+    public static void getMaterias(String token, Response.Listener<JSONObject> successListener, Response.ErrorListener errorListener){
+        String url = BASE_URL + "materias/addHorario";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, successListener, errorListener){
+            @Override
+            public Map<String, String> getHeaders(){
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        peticion.add(request);
+    }
+
+    public static void getActividades(String token, Response.Listener<JSONObject> succesListener, Response.ErrorListener errorListener){
+        String url = BASE_URL + "actividades";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, succesListener, errorListener){
+          @Override
+          public Map<String, String> getHeaders(){
+              Map<String, String> headers = new HashMap<>();
+              headers.put("Authorization", "Bearer "+ token);
+              return headers;
+          }
+        };
+        peticion.add(request);
+    }
     public static void postCreateRegister(String token, RegisterContent content, Response.Listener<JSONObject> successListener, Response.ErrorListener errorListener) throws JSONException {
         String url = BASE_URL + "registros/create/";
 
         JSONObject jsonContent = new JSONObject();
 
         jsonContent.put("idHorario", content.getIdHorario());
-        jsonContent.put("actividad", content.getActividad());
+        jsonContent.put("idActividad", content.getActividad());
         jsonContent.put("laboratorio", content.getLabName());
         JsonObjectRequest createRequest = new JsonObjectRequest(Request.Method.POST, url,jsonContent,successListener,errorListener){
             @Override
@@ -113,7 +156,30 @@ public class Peticiones {
         };
         peticion.add(createRequest);
     }
+    public static void postCreateRegisterWithoutSchedule(String token, SinRegisterContent content, Response.Listener<JSONObject> succesListener, Response.ErrorListener errorListener ) throws JSONException {
+        String url = BASE_URL + "registros/create/whitout";
 
+        JSONObject jsonContent = new JSONObject();
+
+        jsonContent.put("idGrupo", content.getIdGrupo());
+        jsonContent.put("idMateria", content.getIdMateria());
+        jsonContent.put("inicia", content.getInicia());
+        jsonContent.put("finaliza", content.getFinaliza());
+        jsonContent.put("dia", content.getDia());
+        jsonContent.put("idActividad", content.getIdActividad());
+        jsonContent.put("laboratorio", content.getLab());
+        jsonContent.put("idUsuario", content.getIdUsuario());
+
+        JsonObjectRequest createRequest = new JsonObjectRequest(Request.Method.POST, url, jsonContent, succesListener, errorListener){
+            @Override
+            public Map<String, String> getHeaders(){
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer "+ token);
+                return headers;
+            }
+        };
+        peticion.add(createRequest);
+    }
     public static void postAlumnosAtendidos(String token,int idRegister, int alumnos, Response.Listener<JSONObject> succesListener, Response.ErrorListener errorListener) throws JSONException {
         String url = BASE_URL + "registros/complete/" + idRegister;
         JSONObject alumnosData = new JSONObject();
@@ -147,9 +213,10 @@ public class Peticiones {
         String url = BASE_URL + "reportes";
 
         JSONObject data = new JSONObject();
-        data.put("usuario",reporte.getIdUsuario());
-        data.put("titulo",reporte.getTitulo());
-        data.put("problema", reporte.getProblema());
+        data.put("idLab",reporte.getLaboratorio());
+        data.put("idUsuario",reporte.getIdUsuario());
+        data.put("problema",reporte.getTitulo());
+        data.put("descripcion", reporte.getProblema());
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url,data,successListener, errorListener){
             @Override
             public Map<String, String> getHeaders(){
@@ -160,18 +227,18 @@ public class Peticiones {
         };
         peticion.add(request);
     }
-    public static void setToken(Context context, String token){
+    public static void setPreference(Context context, String name, String parameter){
         @SuppressLint("CommitPrefEdits") SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
-        editor.putString("token",token);
+        editor.putString(name,parameter);
         editor.apply();
     }
 
-    public static String getToken(Context context){
+    public static String getPreference(Context context, String name){
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        String token = preferences.getString("token",null);
-        if (token == null || token.isEmpty()){
-            token = "error";
+        String preference = preferences.getString(name,null);
+        if (preference == null || preference.isEmpty()){
+            preference = "error";
         }
-        return  token;
+        return  preference;
     }
 }
